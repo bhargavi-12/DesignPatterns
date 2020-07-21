3c5b037bf2eb41f18646aa1fbbfd3fe539fb9adb
Konda-Akhila <akhilaappu68@gmail.com>
2020-07-21 19:19:50 +0530

    First commit

---
+++ Buildedp/src/main/java/builder/Buildedp/App.java
@@ -0,0 +1,13 @@
+package builder.Buildedp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
